da0fe2909d2ee241836e327c0c63db081de4a073
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2023-07-09 04:16:42 +0200

    adding   jwt

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -98,10 +98,10 @@ const HeroSection = () => {
   return (
     <section className="bg-white">
       <div className="container mx-auto py-10 px-4">
-            <div class="bg-blue-500 py-16 rounded">
-        <div class="container mx-auto px-4">
-          <h1 class="text-4xl font-bold text-white mb-6">Welcome to the Hero Section</h1>
-          <p class="text-lg text-white max-w-xl">
+            <div className="bg-blue-500 py-16 rounded">
+        <div className="container mx-auto px-4">
+          <h1 className="text-4xl font-bold text-white mb-6">Welcome to the Hero Section</h1>
+          <p className="text-lg text-white max-w-xl">
             Hey there! How are you doing? I hope you're having a fantastic day. It's always great to connect with new people and have meaningful conversations. If you have any questions or need assistance, feel free to reach out. Remember to stay positive and keep smiling!
           </p>
         </div>
@@ -162,27 +162,27 @@ const HeroSection = () => {
 
 
       </div>
-      <div class="shadow-xl mt-8 mr-0 mb-0 ml-0 pt-4 pr-10 pb-4 pl-10 flow-root rounded-lg sm:py-2">
-        <div class="pt--10 pr-0 pb-10 pl-0">
+      <div className="shadow-xl mt-8 mr-0 mb-0 ml-0 pt-4 pr-10 pb-4 pl-10 flow-root rounded-lg sm:py-2">
+        <div className="pt--10 pr-0 pb-10 pl-0">
           {!isHidden && (
-            <div class="pt-5 pr-0 pb-0 pl-0 mt-5 mr-0 mb-0 ml-0 ">
-               <div class="">
-          <p class="text-xl font-bold text-gray-900">Open Positions</p>
-          <p class="text-sm mt-1 mr-0 mb-0 ml-0 font-semi-bold text-gray-500">Lorem ipsum dolor sit amet, consectetur adipis</p>
+            <div className="pt-5 pr-0 pb-0 pl-0 mt-5 mr-0 mb-0 ml-0 ">
+               <div className="">
+          <p className="text-xl font-bold text-gray-900">Open Positions</p>
+          <p className="text-sm mt-1 mr-0 mb-0 ml-0 font-semi-bold text-gray-500">Lorem ipsum dolor sit amet, consectetur adipis</p>
         </div>
               {sortedJobs.length > 0 ? (
                 sortedJobs.slice(0, 10).map((job) => (
-                  <div key={job.slug} class="pt-5 pr-0 pb-0 pl-0 mt-5 mr-0 mb-0 ml-0">
-                    <div class="sm:flex sm:items-center sm:justify-between sm:space-x-5 rounded shadow-sm">
-                      <div class="flex items-center flex-1 min-w-0">
-                        <img src='https://v1.tailwindcss.com/_next/static/media/tailwind-ui-sidebar.2ccd3a8ec5f31f428204b5c3c4d9a485.png' class="flex-shrink-0 object-cover rounded-full btn- w-10 h-10" />
-                        <div class="mt-0 mr-0 mb-0 ml-4 flex-1 min-w-0">
-                          <p class="text-lg font-bold text-gray-800 truncate">{job.title}</p>
-                          <p class="text-gray-600 text-md">{job.company_name}</p>
+                  <div key={job.slug} className="pt-5 pr-0 pb-0 pl-0 mt-5 mr-0 mb-0 ml-0">
+                    <div className="sm:flex sm:items-center sm:justify-between sm:space-x-5 rounded shadow-sm">
+                      <div className="flex items-center flex-1 min-w-0">
+                        <img src='https://v1.tailwindcss.com/_next/static/media/tailwind-ui-sidebar.2ccd3a8ec5f31f428204b5c3c4d9a485.png' className="flex-shrink-0 object-cover rounded-full btn- w-10 h-10" />
+                        <div className="mt-0 mr-0 mb-0 ml-4 flex-1 min-w-0">
+                          <p className="text-lg font-bold text-gray-800 truncate">{job.title}</p>
+                          <p className="text-gray-600 text-md">{job.company_name}</p>
                         </div>
                       </div>
-                      <div class="mt-4 mr-0 mb-0 ml-0 pt-0 pr-0 pb-0 pl-14 flex items-center sm:space-x-6 sm:pl-0 sm:mt-0">
-                        <button onClick={handleApplyNow} class="bg-gray-800 pt-2 pr-6 pb-2 pl-6 text-lg font-medium text-gray-100 transition-all duration-200 hover:bg-gray-700 rounded-lg">Apply</button>
+                      <div className="mt-4 mr-0 mb-0 ml-0 pt-0 pr-0 pb-0 pl-14 flex items-center sm:space-x-6 sm:pl-0 sm:mt-0">
+                        <button onClick={handleApplyNow} className="bg-gray-800 pt-2 pr-6 pb-2 pl-6 text-lg font-medium text-gray-100 transition-all duration-200 hover:bg-gray-700 rounded-lg">Apply</button>
                       </div>
                     </div>
                   </div>
--- frontend/src/pages/saved-jobs.js
+++ frontend/src/pages/saved-jobs.js
@@ -1,34 +1,87 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Layout from '../components/Layouts';
-import JobCard from '../components/JobCard';
 
-const SavedJobsPage = () => {
+const JobPage = () => {
+  const [job, setJob] = useState(null);
   const [savedJobs, setSavedJobs] = useState([]);
+  const [userId, setUserId] = useState(''); // Assuming you have a way to get the user ID
 
   useEffect(() => {
-    // Fetch user's saved jobs from backend API
-    const fetchSavedJobs = async () => {
-      try {
-        const response = await axios.get('http://localhost:5000/api/saved');
-        setSavedJobs(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
     fetchSavedJobs();
-  }, []);
+  }, [userId]); // Fetch saved jobs whenever the user ID changes
+
+  // Assuming you have a way to get the user ID
+
+
+// Update the handleSaveJob function
+const handleSaveJob = async () => {
+  if (job && userId) { // Check if userId is non-empty
+    try {
+      await axios.post('http://localhost:5000/api/jobs/save', {
+        userId,
+        job,
+      });
+      fetchSavedJobs();
+    } catch (error) {
+      console.error('Error saving job:', error);
+    }
+  } else {
+    console.error('User ID is missing or empty.'); // Handle the case when userId is missing or empty
+  }
+};
+
+  const fetchSavedJobs = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/api/jobs/saved/${userId}`);
+      const savedJobsData = response.data;
+      setSavedJobs(savedJobsData);
+    } catch (error) {
+      console.error('Error fetching saved jobs:', error);
+    }
+  };
+
+  const handleFetchJob = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/jobs');
+      const jobData = response.data.data;
+      console.log(typeof jobsData)
+      // Assuming you want to fetch the first job from the API response
+      const firstJob = jobData[0];
+
+      setJob(firstJob);
+    } catch (error) {
+      console.error('Error fetching job:', error);
+    }
+  };
 
   return (
-    <Layout>
-      <div>
-        <h1>Saved Jobs</h1>
-        {/* Render saved job list */}
-        <JobCard jobs={savedJobs} />
-      </div>
-    </Layout>
+    <div>
+      <h2>Job Details</h2>
+      {job ? (
+        <div className="job">
+          <h3>{job.title}</h3>
+          <p>{job.company_name}</p>
+          <p>{job.location}</p>
+          {/* Render other job details */}
+          <button onClick={handleSaveJob}>Save Job</button>
+        </div>
+      ) : (
+        <p>No job available.</p>
+      )}
+
+      <h2>Saved Jobs</h2>
+      {savedJobs.map((savedJob) => (
+        <div key={savedJob.slug} className="saved-job">
+          <h3>{savedJob.title}</h3>
+          <p>{savedJob.company_name}</p>
+          <p>{savedJob.location}</p>
+          {/* Render other saved job details */}
+        </div>
+      ))}
+
+      <button onClick={handleFetchJob}>Fetch Job</button>
+    </div>
   );
 };
 
-export default SavedJobsPage;
+export default JobPage;
